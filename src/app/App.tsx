5d328a76273e509cbbcdbb1ab5ce10be0ed56397
Spudisis <alekasisbb@gmail.com>
2023-04-08 19:55:52 +0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+// import { Router } from 'router/index';
+
+export function App() {
+  return <div>dsfhdsf</div>; // <BrowserRouter>
+  //   <CssBaseline />
+  //   <Router />
+  // </BrowserRouter>
+}
