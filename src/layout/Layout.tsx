bfbd9e53c1a42a6169fd5a2d7da2f56a6b470596
Rystem <2starcraft@list.ru>
2023-04-08 22:46:33 +0300

    fix

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Footer } from './footer/footer';
-import { Header } from './header/header';
+import { Footer } from './footer/Footer';
+import { Header } from './header/Header';
 
 type Layout = {
   children: React.ReactNode;
