94137376acf73bb6056746c47d443427e5ee1c8a
Spudisis <alekasisbb@gmail.com>
2023-04-09 14:00:48 +0300

    store

---
+++ src/pages/Profile/store/store.ts
@@ -0,0 +1,24 @@
+import { makeAutoObservable } from "mobx";
+import { InstanceUser } from "../../../http/Agent/User.agent";
+
+class Profile {
+    constructor(){
+        makeAutoObservable({})
+    }
+    changePassword = async ()=>{
+
+    }
+    changeDataUser = async ()=>{
+
+    }
+    changeLogoUser = async (id:number, data:any)=>{
+        try {
+           await InstanceUser.setNewAvatar(id, data)
+        } catch {
+            
+        }
+        
+    }
+}
+
+export const ProfileStore = new Profile()
