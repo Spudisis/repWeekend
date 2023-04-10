14d826f0ecbe3edb687a9d00a3905322ace4419b
Spudisis <alekasisbb@gmail.com>
2023-04-10 23:00:43 +0300

    hsdfhds

--- src/http/Agent/Basic.agent.ts
+++ src/http/Agent/Basic.agent.ts
@@ -4,33 +4,31 @@ export class BasicAgent {
   protected _http: AxiosInstance;
   constructor(baseURL: string, config?: AxiosRequestConfig) {
     this._http = axios.create({
-      
       baseURL,
       ...config,
     });
-    this._http.interceptors.request.use((config)=>{
-        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-        return config;
-    })
-    this._http.interceptors.response.use((config)=>{
-      
+    this._http.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
       return config;
-      
-  }, async (error)=>{
-    const originRequest = error.config
-    if (error.response.status===401 && error.config && !error.config._isRetry){
-      originRequest._isRetry = true
-      try {
-        
-        const { data } = await axios.post<any>(`/auth/refresh`);
-        localStorage.setItem('token', data.access_token)
-        return this._http.request(originRequest)
-      } catch(e:any) {
-        console.log('пользователь не атворизован')
-      }
-     
-    }
-    throw error
-  })
-    
-}}
+    });
+    this._http.interceptors.response.use(
+      (config) => {
+        return config;
+      },
+      async (error) => {
+        const originRequest = error.config;
+        if (error.response.status === 401 && error.config && !error.config._isRetry) {
+          originRequest._isRetry = true;
+          try {
+            const { data } = await axios.post<any>(`${import.meta.env.VITE_APP_API as string}/auth/refresh`);
+            localStorage.setItem('token', data.access_token);
+            return this._http.request(originRequest);
+          } catch (e: any) {
+            console.log('пользователь не атворизован');
+          }
+        }
+        throw error;
+      },
+    );
+  }
+}
--- src/http/Agent/Login.agent.ts
+++ src/http/Agent/Login.agent.ts
@@ -1,33 +1,32 @@
-import axios from "axios";
-import { BasicAgent } from "./Basic.agent";
-import {setAuthTokens} from 'axios-jwt';
-class Login extends BasicAgent{
-    constructor(){
-        super(import.meta.env.VITE_APP_API as string)
-    }
-    async authUser(body:any): Promise<any> {
-        const { data } = await this._http.post<any>(`/auth/login`, body);
-        console.log(data)
-        localStorage.setItem('token', data.access_token)
-        return data;
-    }
-    async refreshToken(): Promise<any> {
-        const { data } = await axios.post<any>(`/auth/refresh`);
-        console.log(data)
-       
-        localStorage.setItem('token', data.access_token)
-        return data;
-    }
-    async getInfoMe(): Promise<any> {
-        const { data } = await this._http.get<any>(`/auth/me`);
-        console.log(data)
-        return data;
-    }
-    async getCaptchaLogin(): Promise<any> {
-        const { data } = await this._http.get<any>(`/auth/captcha`);
-        console.log(data)
-        return data;
-    }
+import axios from 'axios';
+import { BasicAgent } from './Basic.agent';
+import { setAuthTokens } from 'axios-jwt';
+class Login extends BasicAgent {
+  constructor() {
+    super(import.meta.env.VITE_APP_API as string);
+  }
+  async authUser(body: any): Promise<any> {
+    const { data } = await this._http.post<any>(`/auth/login`, body);
+    console.log(data);
+    localStorage.setItem('token', data.access_token);
+    return data;
+  }
+  async refreshToken(): Promise<any> {
+    const { data } = await axios.post<any>(`/auth/refresh`);
+    console.log(data);
+
+    return data;
+  }
+  async getInfoMe(): Promise<any> {
+    const { data } = await this._http.get<any>(`/auth/me`);
+    console.log(data);
+    return data;
+  }
+  async getCaptchaLogin(): Promise<any> {
+    const { data } = await this._http.get<any>(`/auth/captcha`);
+    console.log(data);
+    return data;
+  }
 }
 
-export const InstanceLogin = new Login()
+export const InstanceLogin = new Login();
